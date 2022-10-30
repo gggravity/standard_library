ca5a9e0ad00b4148df544d4631ee0131eebf0e38
martin <m@rtin.bz>
2022-10-30 11:20:40 -0300

    sharedptr2

---
+++ util/sharedptr2.cpp
@@ -0,0 +1,37 @@
+//
+// Created by martin on 10/30/22.
+//
+
+#pragma once
+
+#include <bits/stdc++.h>
+
+#include <utility>
+
+using namespace std;
+
+class File_deleter
+{
+ public:
+  explicit File_deleter (string fn) :
+      filename { std::move(fn) }
+  {
+
+  }
+
+  void operator() (ofstream *fp)
+  {
+    delete fp;
+    remove(filename.c_str());
+  }
+
+ private:
+  string filename;
+};
+
+int main ()
+{
+
+  shared_ptr<ofstream> fp(new ofstream("tmp_file.txt"), File_deleter("tmp_file.txt"));
+  sleep(10);
+}
