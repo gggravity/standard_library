db1dbcf475d8efcc5ee2de15b1fc77fdf8b1ab91
martin <m@rtin.bz>
2022-10-31 19:32:38 -0300

    sdl

---
+++ stl/unordmap1.cpp
@@ -0,0 +1,27 @@
+//
+// Created by martin on 10/31/22.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main ()
+{
+  unordered_map<string, double> coll {
+      { "tim", 9.9 },
+      { "struppi", 11.77 }
+  };
+
+  for (auto &elem : coll)
+    {
+      elem.second += elem.second;
+    }
+
+  cout << fixed << setprecision(2);
+  
+  for (const auto &elem : coll)
+    {
+      cout << setw(12) << left << elem.first + ": " << elem.second << endl;
+    }
+}
