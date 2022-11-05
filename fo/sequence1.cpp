524a098fac64624a6ba8c8ebac6eabefc8377ef7
martin <m@rtin.bz>
2022-11-05 15:48:49 -0300

    .

---
+++ fo/sequence1.cpp
@@ -0,0 +1,37 @@
+//
+// Created by martin on 11/5/22.
+//
+#include <bits/stdc++.h>
+
+#include <utility>
+#include "../stl/print.hpp"
+
+using namespace std;
+
+struct Int_sequence
+{
+  explicit Int_sequence (int initial_value) :
+      value(initial_value)
+  {
+
+  }
+
+  int operator() ()
+  {
+    return value++;
+  }
+
+ private:
+  int value;
+};
+
+int main ()
+{
+  list<int> coll;
+
+  generate_n(back_inserter(coll), 9, Int_sequence(1));
+  PRINT_ELEMENTS(coll);
+
+  generate(next(coll.begin()), prev(coll.end()), Int_sequence(42));
+  PRINT_ELEMENTS(coll);
+}
