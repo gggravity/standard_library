02ae5c8d5be442706deb336c9fbda20c19b2922e
martin <m@rtin.bz>
2022-11-05 17:53:22 -0300

    .

--- fo/bind1.cpp
+++ fo/bind1.cpp
@@ -6,25 +6,31 @@
 #include "../stl/print.hpp"
 
 using namespace std;
+using namespace placeholders;
 
 int main ()
 {
-  auto plus10 { bind(plus<>(),
-                     placeholders::_1, 10) };
+  auto plus10
+      {
+          bind(plus<>(), _1, 10)
+      };
   cout << "+10:              " << plus10(7) << endl;
 
-  auto plus10time2 { bind(multiplies<>(),
-                          bind(plus<>(),
-                               placeholders::_1, 10), 2) };
+  auto plus10time2
+      {
+          bind(multiplies<>(), bind(plus<>(), _1, 10), 2)
+      };
   cout << "+10 * 2:          " << plus10time2(7) << endl;
 
-  auto pow3 { bind(multiplies<>(),
-                   bind(multiplies<>(),
-                        placeholders::_1,
-                        placeholders::_1),
-                   placeholders::_1) };
+  auto pow3
+      {
+          bind(multiplies<>(), bind(multiplies<>(), _1, _1), _1)
+      };
   cout << "x*x*x:            " << pow3(7) << endl;
 
-  auto inverse_divide { bind(plus<>(), placeholders::_1, 10) };
+  auto inverse_divide
+      {
+          bind(divides<double>(), _2, _1)
+      };
   cout << "inverse_divide:   " << inverse_divide(49, 7) << endl;
 }
