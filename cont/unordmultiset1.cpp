df8bd1fb2b7861168e48fb8d1fd9d675d231de7d
martin <m@rtin.bz>
2022-11-04 22:05:29 -0300

    .

---
+++ cont/unordmultiset1.cpp
@@ -0,0 +1,25 @@
+//
+// Created by martin on 11/4/22.
+//
+#include <bits/stdc++.h>
+#include "../stl/print.hpp"
+
+using namespace std;
+
+int main ()
+{
+  unordered_multiset<int> coll { 1, 2, 3, 5, 7, 11, 13, 17, 19, 77 };
+  coll.insert({ -7, 17, 33, -11, 17, 19, 1, 13 });
+  PRINT_ELEMENTS(coll);
+
+  coll.erase(17);
+
+  auto pos { coll.find(13) };
+
+  if (pos != coll.end())
+    {
+      coll.erase(pos);
+    }
+
+  PRINT_ELEMENTS(coll);
+}
