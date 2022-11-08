6fbfa8174b597809f4ae28f2dd05eae833de0848
martin <m@rtin.bz>
2022-11-08 19:39:33 -0300

    .

---
+++ algo/adjacentdiff1.cpp
@@ -0,0 +1,35 @@
+//
+// Created by martin on 11/8/22.
+//
+
+#include <bits/stdc++.h>
+#include "algostuff.hpp"
+#include "../stl/print.hpp"
+//#include "../algorithms.hpp"
+
+#define RANGE(x) (x).begin(), (x).end()
+#define CONST_RANGE(x) (x).cbegin(), (x).cend()
+
+using namespace std;
+
+int main ()
+{
+  vector<int> coll;
+//  coll = { 1, -1, 2, -2, 3, -3 };
+  INSERT_ELEMENTS(coll, 1, 6);
+  PRINT_ELEMENTS(coll);
+
+  adjacent_difference(CONST_RANGE(coll),
+              ostream_iterator<int>(cout, " "));
+  cout << endl;
+
+  adjacent_difference(CONST_RANGE(coll),
+              ostream_iterator<int>(cout, " "),
+              plus { });
+  cout << endl;
+
+  adjacent_difference(CONST_RANGE(coll),
+                      ostream_iterator<int>(cout, " "),
+                      multiplies { });
+  cout << endl;
+}
