3dec8208e45f1df47e0fe325594af026daa22739
martin <m@rtin.bz>
2022-11-16 17:17:55 -0300

    .

---
+++ io/charcat1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main ()
+{
+  istringstream iss { "der var en gang en mand." };
+  char c;
+
+  while (iss.get(c))
+    {
+      cout.put(c);
+    }
+}
