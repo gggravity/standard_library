f853e7b11bb5c841536749605a900185791da944
martin <m@rtin.bz>
2022-11-21 18:31:45 -0300

    .

---
+++ io/streamreadwrite1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main ()
+{
+  filebuf buffer;
+  ostream output { &buffer };
+  istream input { &buffer };
+
+  buffer.open("../io/example.dat", ios::in | ios::out | ios::trunc);
+
+  for (int i { 1 } ; i <= 4 ; ++i)
+    {
+      output << i << ". line" << endl;
+
+      input.seekg(0);
+      char c;
+      while (input.get(c))
+        {
+          cout.put(c);
+        }
+      cout << endl;
+      input.clear();
+    }
+}
