54670cbd4031ef41ed4b13e9e9883ecaa4ee1b26
martin <m@rtin.bz>
2022-11-25 18:30:04 -0300

    .

---
+++ num/complex2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main ()
+{
+  complex<long double> c1, c2;
+
+  while (cin.peek() != EOF)
+    {
+      cout << "complex number c1: ";
+      cin >> c1;
+      if (!cin)
+        {
+          cerr << "input error" << endl;
+          return EXIT_FAILURE;
+        }
+      cout << "complex number c2: ";
+
+      cin >> c2;
+      if (!cin)
+        {
+          cerr << "input error" << endl;
+          return EXIT_FAILURE;
+        }
+
+      if (c1 == c2)
+        {
+          cout << "c1 and c2 are equal !" << endl;
+        }
+
+      cout << "c1 raised to the c2 power: " << pow(c1, c2) << endl;
+
+      cin.ignore(numeric_limits<int>::max(), '\n');
+    }
+
+}
